37d57ab7f3b3f808e96718af5e75c66c1f591468
minalz <119687281@qq.com>
2021-06-04 22:23:37 +0800

    更新小岛数量的解法

--- src/test/java/cn/minalz/bfs/BFSDemo1.java
+++ src/test/java/cn/minalz/bfs/BFSDemo1.java
@@ -62,17 +62,16 @@ public class BFSDemo1 {
                 {'0','0','0','0','0'}
         };
         System.out.println(numIslands(grid));
-    }
 
-    @Test
-    public void test02() {
-        char[][] grid = {
+        System.out.println("---------------");
+
+        char[][] grid2 = {
                 {'1','1','0','0','0'},
                 {'1','1','0','1','0'},
                 {'0','0','0','0','0'},
                 {'0','0','0','1','1'}
         };
-        System.out.println(numIslands(grid));
+        System.out.println(numIslands(grid2));
     }
 
     /**
@@ -125,6 +124,55 @@ public class BFSDemo1 {
                 }
             }
         }
+    }
+
+    @Test
+    public void test02() {
+        char[][] grid = {
+                {'1','1','0','0','0'},
+                {'1','1','0','1','0'},
+                {'0','0','0','0','0'},
+                {'0','0','0','1','1'}
+        };
+        System.out.println(numIslands2(grid));
+    }
+
+    /**
+     * 第三种方式 执行速度更快
+     */
+    public void dfs3(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs3(grid, r - 1, c);
+        dfs3(grid, r + 1, c);
+        dfs3(grid, r, c - 1);
+        dfs3(grid, r, c + 1);
+    }
+
+    public int numIslands3(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs3(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
     }
 
     @Test
