80af0c7e3d3484dbb278f11654107adee3ef695c
minalz <119687281@qq.com>
2021-06-09 00:49:00 +0800

    添加demo test

---
+++ src/test/java/cn/minalz/demo/DemoTest1.java
@@ -0,0 +1,44 @@
+package cn.minalz.demo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @description:
+ * @author: minalz
+ * @date: 2021-06-08 22:27
+ **/
+public class DemoTest1 {
+
+    /**
+     * 测试
+     */
+    @Test
+    public void testSort() {
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = (int)(Math.random()*10000);
+        }
+
+        bubbleSort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
+    }
+
+    /**
+     * 冒泡排序
+     * @param array
+     */
+    public void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
